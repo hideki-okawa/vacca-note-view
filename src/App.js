a86bb983035230fd1a94c1584e14eee704ba8240
Okaki030 <h.okawa0303@gmail.com>
2021-08-15 11:31:31 +0900

    #1 ファイルの整理

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import logo from './logo.svg';
 import './App.css';
 
 import axios from "axios";
 
-function App() {
+const App = () => {
   const [comment, setComment] = useState("");
 
   useEffect(() => {
@@ -21,10 +20,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
