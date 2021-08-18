4ba345fe12c41143d930ed554a06df0d1a990277
Okaki030 <h.okawa0303@gmail.com>
2021-08-18 22:04:27 +0900

    #3 カードにAPIレスポンスの内容を適応

--- src/components/uiParts/notes-view-list-content-card/index.js
+++ src/components/uiParts/notes-view-list-content-card/index.js
@@ -1,8 +1,129 @@
-import React from "react";
+import React, { useEffect } from "react";
 import man from "~/img/user-man.png";
 import woman from "~/img/user-woman.png";
 
 import { Grid, Segment, Header, Image } from "semantic-ui-react";
+
+const setGendar = (genderNum) => {
+	let gender;
+	switch (genderNum) {
+		case "1":
+			gender = "男性";
+			break;
+		case "2":
+			gender = "女性";
+			break;
+		default:
+			gender = "性別不明";
+	}
+	return gender;
+};
+
+const setImage = (genderNum) => {
+	let image;
+	switch (genderNum) {
+		case "1":
+			image = man;
+			break;
+		case "2":
+			image = woman;
+			break;
+		default:
+			image = "";
+	}
+	return image;
+};
+
+const setAge = (ageNum) => {
+	let age;
+	switch (ageNum) {
+		case "1":
+			age = "10代以下";
+			break;
+		case "2":
+			age = "20代";
+			break;
+		case "3":
+			age = "30代";
+			break;
+		case "4":
+			age = "40代";
+			break;
+		case "5":
+			age = "50代";
+			break;
+		case "6":
+			age = "60代";
+			break;
+		case "7":
+			age = "70代";
+			break;
+		case "8":
+			age = "80代";
+			break;
+		case "9":
+			age = "90代以上";
+			break;
+		default:
+			age = "年齢不明";
+	}
+	return age;
+};
+
+const setVaccineType = (vaccineTypeSymbol) => {
+	let vaccineType;
+	switch (vaccineTypeSymbol) {
+		case "M":
+			vaccineType = "モデルナ";
+			break;
+		case "F":
+			vaccineType = "ファイザー";
+			break;
+		case "A":
+			vaccineType = "アストラゼネカ";
+			break;
+		default:
+			vaccineType = "ワクチン種不明";
+	}
+	return vaccineType;
+};
+
+const setNumberOfVaccination = (numberOfVaccinationNum) => {
+	return numberOfVaccinationNum + "回目";
+};
+
+const setMaxTemperature = (maxTemperatureNum) => {
+	let maxTemperature;
+	switch (maxTemperatureNum) {
+		case "1":
+			maxTemperature = "36.9度以下";
+			break;
+		case "2":
+			maxTemperature = "37.0~37.4";
+			break;
+		case "3":
+			maxTemperature = "37.5~37.9";
+			break;
+		case "4":
+			maxTemperature = "38.0~38.4";
+			break;
+		case "5":
+			maxTemperature = "38.5~38.9";
+			break;
+		case "6":
+			maxTemperature = "39.0~39.4";
+			break;
+		case "7":
+			maxTemperature = "39.5~39.9";
+			break;
+		case "8":
+			maxTemperature = "40度以上";
+			break;
+		default:
+			maxTemperature = "不明";
+	}
+	return maxTemperature + "℃";
+};
 
 const NotesViewListContentCard = (props) => {
 	let note = props.note;
@@ -11,13 +132,16 @@ const NotesViewListContentCard = (props) => {
 		<Grid.Column>
 			<Segment raised>
 				<Header as="h3">
-					<Image src={man} />
+					<Image src={setImage(note.gender)} />
 					<Header.Content>
-						{`${note.gender}/${note.age}/${note.vaccine_type}/$
-								{note.number_of_vaccination}/${note.max_temperature}`}
+						{`${setGendar(note.gender)}/${setAge(note.age)}/${setVaccineType(
+							note.vaccine_type
+						)}/${setNumberOfVaccination(
+							note.number_of_vaccination
+						)}/${setMaxTemperature(note.max_temperature)}`}
 					</Header.Content>
 				</Header>
-				<p className="note-paragraph"></p>
+				<p className="note-paragraph">{note.log}</p>
 			</Segment>
 		</Grid.Column>
 	);
