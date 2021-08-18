c8bbb69e1a094ca9e40356b011d6b59e2499f981
Okaki030 <h.okawa0303@gmail.com>
2021-08-18 20:51:11 +0900

    #3 api呼び出しの実装

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.scss";
 import Header from "~/components/uiParts/header/index.js";
 import Top from "~/components/pages/top/index.js";
 
-import axios from "axios";
-
 const App = () => {
-	const [comment, setComment] = useState("");
-
-	useEffect(() => {
-		const api = process.env.REACT_APP_SERVER_API;
-		const fetchData = async () => {
-			const response = await axios(api);
-			setComment(response.data);
-		};
-		fetchData();
-	}, []);
-
 	return (
 		<div className="App">
 			<div className="title-header">
--- src/components/uiParts/notes-view-list-content/index.js
+++ src/components/uiParts/notes-view-list-content/index.js
@@ -1,33 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.scss";
 import man from "~/img/user-man.png";
 import woman from "~/img/user-woman.png";
 
 import { Grid, Segment, Header, Image } from "semantic-ui-react";
+import axios from "axios";
 
 const NotesViewListContent = () => {
+	const [notes, setNotes] = useState([]);
+
+	useEffect(() => {
+		const api = process.env.REACT_APP_SERVER_API + "/notes";
+		const fetchData = async () => {
+			const response = await axios(api);
+			setNotes(response.data);
+		};
+		fetchData();
+	}, []);
+
 	return (
 		<Grid columns={2} stackable>
-			<Grid.Column>
-				<Segment raised>
-					<Header as="h3">
-						<Image src={man} />
-						<Header.Content>男性/20代/ファイザー/1回目/36.5℃</Header.Content>
-					</Header>
-					<p className="note-paragraph">
-						2021/08/06 11:25 接種
-						<br /> 13:08 左腕に若干の痺れを感じる
-						<br /> 14:08 左腕が少し上がらなくなってきた
-						<br /> 18:39 左腕が肩より上に上がらない
-						<br />
-						20:26 37.3
-						<br />
-						2021/08/07 4:14 36.5 全身に痛みを感じる 頭痛あり
-						<br />
-						6:15 36.6 全体的に痛みが引く
-					</p>
-				</Segment>
-			</Grid.Column>
+			{notes.map((note) => {
+				return (
+					<Grid.Column>
+						<Segment raised>
+							<Header as="h3">
+								<Image src={man} />
+								{/* <Header.Content>男性/20代/ファイザー/1回目/36.5℃</Header.Content> */}
+								<Header.Content>
+									{`${note.gender}/${note.age}/${note.vaccine_type}/$
+								{note.number_of_vaccination}/${note.max_temperature}`}
+								</Header.Content>
+							</Header>
+							<p className="note-paragraph"></p>
+						</Segment>
+					</Grid.Column>
+				);
+			})}
 		</Grid>
 	);
 };
