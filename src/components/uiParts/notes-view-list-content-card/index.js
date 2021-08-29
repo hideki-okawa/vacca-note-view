d9599fa027c1fc8fc227c7aa0cc21acce0442d6f
Okaki030 <h.okawa0303@gmail.com>
2021-08-29 13:34:03 +0900

    #9 接種体験一覧ページの修正

--- src/components/uiParts/notes-view-list-content-card/index.js
+++ src/components/uiParts/notes-view-list-content-card/index.js
@@ -2,22 +2,16 @@ import React from "react";
 import man from "~/img/user-man.png";
 import woman from "~/img/user-woman.png";
 import SeeNextButton from "~/components/uiParts/see-next-button/index.js";
+import * as constants from "~/constants.js";
 
 import { Grid, Segment, Header, Image } from "semantic-ui-react";
 
-const setGendar = (genderNum) => {
-	let gender;
-	switch (genderNum) {
-		case "1":
-			gender = "男性";
-			break;
-		case "2":
-			gender = "女性";
-			break;
-		default:
-			gender = "性別不明";
+const setGender = (genderNum) => {
+	if (constants.GENDER_TYPE[genderNum]) {
+		return constants.GENDER_TYPE[genderNum];
+	} else {
+		return "性別不明";
 	}
-	return gender;
 };
 
 const setImage = (genderNum) => {
@@ -36,101 +30,35 @@ const setImage = (genderNum) => {
 };
 
 const setAge = (ageNum) => {
-	let age;
-	switch (ageNum) {
-		case "1":
-			age = "10代以下";
-			break;
-		case "2":
-			age = "20代";
-			break;
-		case "3":
-			age = "30代";
-			break;
-		case "4":
-			age = "40代";
-			break;
-		case "5":
-			age = "50代";
-			break;
-		case "6":
-			age = "60代";
-			break;
-		case "7":
-			age = "70代";
-			break;
-		case "8":
-			age = "80代";
-			break;
-		case "9":
-			age = "90代以上";
-			break;
-		default:
-			age = "年齢不明";
+	if (constants.AGE_TYPE[ageNum]) {
+		return constants.AGE_TYPE[ageNum];
+	} else {
+		return "年齢不明";
 	}
-	return age;
 };
 
 const setVaccineType = (vaccineTypeSymbol) => {
-	let vaccineType;
-	switch (vaccineTypeSymbol) {
-		case "M":
-			vaccineType = "モデルナ";
-			break;
-		case "F":
-			vaccineType = "ファイザー";
-			break;
-		case "A":
-			vaccineType = "アストラゼネカ";
-			break;
-		default:
-			vaccineType = "ワクチン種不明";
+	if (constants.VACCINE_TYPE[vaccineTypeSymbol]) {
+		return constants.VACCINE_TYPE[vaccineTypeSymbol];
+	} else {
+		return "ワクチン種不明";
 	}
-	return vaccineType;
 };
 
 const setNumberOfVaccination = (numberOfVaccinationNum) => {
-	if (Number.isInteger(numberOfVaccinationNum)) {
-		return numberOfVaccinationNum + "回目";
+	if (constants.NUMBER_OF_VACCINATION[numberOfVaccinationNum]) {
+		return constants.NUMBER_OF_VACCINATION[numberOfVaccinationNum];
 	} else {
 		return "回数不明";
 	}
 };
 
 const setMaxTemperature = (maxTemperatureNum) => {
-	let maxTemperature;
-	switch (maxTemperatureNum) {
-		case "1":
-			maxTemperature = "36.9℃以下";
-			break;
-		case "2":
-			maxTemperature = "37.0~37.4℃";
-			break;
-		case "3":
-			maxTemperature = "37.5~37.9℃";
-			break;
-		case "4":
-			maxTemperature = "38.0~38.4℃";
-			break;
-		case "5":
-			maxTemperature = "38.5~38.9℃";
-			break;
-		case "6":
-			maxTemperature = "39.0~39.4℃";
-			break;
-		case "7":
-			maxTemperature = "39.5~39.9℃";
-			break;
-		case "8":
-			maxTemperature = "40℃以上";
-			break;
-		case "10":
-			maxTemperature = "発熱なしのため未検温";
-			break;
-		default:
-			maxTemperature = "不明";
+	if (constants.MAX_TEMPERATURE[maxTemperatureNum]) {
+		return constants.MAX_TEMPERATURE[maxTemperatureNum];
+	} else {
+		return "最高体温不明";
 	}
-	return maxTemperature;
 };
 
 const NotesViewListContentCard = (props) => {
@@ -142,7 +70,7 @@ const NotesViewListContentCard = (props) => {
 				<Header as="h3">
 					<Image src={setImage(note.gender)} />
 					<Header.Content>
-						{`${setGendar(note.gender)}/${setAge(note.age)}/${setVaccineType(
+						{`${setGender(note.gender)}/${setAge(note.age)}/${setVaccineType(
 							note.vaccine_type
 						)}/${setNumberOfVaccination(
 							note.number_of_vaccination
