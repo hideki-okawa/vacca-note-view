2fd3a23d7bd1b7780bfdf00cc135c3c8ddf6aaad
Okaki030 <h.okawa0303@gmail.com>
2021-09-02 22:16:50 +0900

    #17 ヘッダーレスポンシブ対応

--- src/components/uiParts/header/index.js
+++ src/components/uiParts/header/index.js
@@ -2,29 +2,41 @@ import React from "react";
 import "./index.scss";
 import logo from "~/img/vacca-note-logo.png";
 
-import { Icon, Grid, Image } from "semantic-ui-react";
+import { Icon, Image, Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import useMedia from "use-media";
 
 const Header = () => {
+	const isWide = useMedia({ minWidth: "767px" });
+
+	const pcImage = (
+		<Image
+			src={logo}
+			className="title-logo-size title-logo-position"
+			alt="logo"
+		/>
+	);
+
+	const smpImage = (
+		<Image
+			src={logo}
+			className="title-logo-smp-size title-logo-position"
+			alt="logo"
+		/>
+	);
+
 	return (
 		<header className="header">
-			<Grid>
-				<Grid.Column width={9}>
-					<Link to="/">
-						<Image src={logo} className="title-logo" alt="logo" />
-					</Link>
-				</Grid.Column>
-				<Grid.Column
-					floated="right"
-					width={1}
-					verticalAlign="middle"
-					// onClick={() =>{
-					//   setActiveSideBar(true)
-					// }}
-				>
-					<Icon name="sidebar" />
-				</Grid.Column>
-			</Grid>
+			<Menu fixed="top" text>
+				<Menu.Item header>
+					<Link to="/">{isWide ? pcImage : smpImage}</Link>
+				</Menu.Item>
+				<Menu.Item position="right">
+					<div className="sidebar-position">
+						<Icon name="sidebar" />
+					</div>
+				</Menu.Item>
+			</Menu>
 		</header>
 	);
 };
