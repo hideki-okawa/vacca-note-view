83171b1cbd68c04e59c885ffd26d29098cdeebd6
Okaki030 <h.okawa0303@gmail.com>
2021-09-09 23:06:50 +0900

    #27 この接種体験もおすすめ機能の実装

---
+++ src/components/uiParts/note-recommend/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+import NotesViewListContentCard from "~/components/uiParts/notes-view-list-content-card/index.js";
+
+import { Card, Header } from "semantic-ui-react";
+import axios from "axios";
+import useMedia from "use-media";
+
+const NoteRecommend = (props) => {
+	const [recommendNotes, setRecommendNotes] = useState([]);
+	const isWide = useMedia({ minWidth: "767px" });
+
+	useEffect(() => {
+		const api = `${process.env.REACT_APP_SERVER_API}/notes/recommend?vaccine_type=${props.vaccineType}&number_of_vaccination=${props.numberOfVaccination}&age=${props.age}`;
+		console.log("api-debug", api);
+		const fetchData = async () => {
+			const response = await axios(api);
+			setRecommendNotes(response.data);
+			console.log("recommendNotes-deubg1", recommendNotes);
+		};
+		fetchData();
+	}, [props.age]);
+
+	let noteRecommendNotes;
+	if (recommendNotes) {
+		noteRecommendNotes = (
+			<div>
+				<div className="note-recommend-header">
+					<Header
+						as={isWide ? "h1" : "h3"}
+						content="あなたにおすすめの接種体験"
+					/>
+				</div>
+				<div className="note-recommend-notes">
+					<Card.Group itemsPerRow={isWide ? 2 : 1}>
+						{recommendNotes.map((note) => {
+							return <NotesViewListContentCard note={note} />;
+						})}
+					</Card.Group>
+				</div>
+			</div>
+		);
+	}
+
+	return <div>{noteRecommendNotes}</div>;
+};
+
+export default NoteRecommend;
