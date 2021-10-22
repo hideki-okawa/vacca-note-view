8d2dd733655715815d15a4ff91940ce84d5b568c
Okaki030 <h.okawa0303@gmail.com>
2021-10-22 09:16:43 +0900

    #65 円グラフを中央寄せ

--- src/components/uiParts/notes-chart/index.js
+++ src/components/uiParts/notes-chart/index.js
@@ -1,7 +1,8 @@
-// import "./styles.css";
 import React, { useCallback, useState } from "react";
-import { PieChart, Pie, Sector, Legend } from "recharts";
-import { Card, Header } from "semantic-ui-react";
+import "./index.scss";
+
+import { PieChart, Pie, Sector, Legend, ResponsiveContainer } from "recharts";
+import { Card } from "semantic-ui-react";
 
 const data = [
 	{ name: "Group A", value: 400 },
@@ -55,22 +56,24 @@ export default function NoteChart() {
 		<Card raised>
 			<Card.Content>
 				<Card.Header>モデルナ(2回目)接種後の体温</Card.Header>
-				<PieChart width={400} height={400}>
-					<Pie
-						activeIndex={activeIndex}
-						activeShape={renderActiveShape}
-						data={data}
-						cx={200}
-						cy={200}
-						innerRadius={60}
-						outerRadius={80}
-						fill="#8884d8"
-						dataKey="value"
-						onMouseEnter={onPieEnter}
-						onMouseLeave={onPieLeave}
-					/>
-					<Legend />
-				</PieChart>
+				<ResponsiveContainer className="notes-chart" width={"80%"} height={400}>
+					<PieChart>
+						<Pie
+							activeIndex={activeIndex}
+							activeShape={renderActiveShape}
+							data={data}
+							cx={"50%"}
+							cy={"50%"}
+							innerRadius={60}
+							outerRadius={80}
+							fill="#8884d8"
+							dataKey="value"
+							onMouseEnter={onPieEnter}
+							onMouseLeave={onPieLeave}
+						/>
+						<Legend />
+					</PieChart>
+				</ResponsiveContainer>
 			</Card.Content>
 		</Card>
 	);
